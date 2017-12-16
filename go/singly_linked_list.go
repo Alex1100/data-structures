796f7b7adb970121f92f600baaad0e284f813467
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-12-16 11:00:43 -0800

    start off singly_linked_list.go

---
+++ go/singly_linked_list.go
@@ -0,0 +1,15 @@
+type SinglyNode struct {
+  Value string
+  Next *SinglyNode
+}
+
+type SinglyLinkedList struct {
+  Head *Node
+  Tail *Node
+  Length int
+}
+
+
+func InitSinglyLikedList() *SinglyLinkedList {
+
+}
