726021ee38dad696b0f1acccb3e234fd9339b27e
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-12-16 11:16:07 -0800

    add basic initialization of SinglyNode and SinglyLinkedList

--- go/singly_linked_list.go
+++ go/singly_linked_list.go
@@ -1,15 +1,51 @@
+package main
+
+import (
+	"fmt"
+)
+
 type SinglyNode struct {
-  Value string
-  Next *SinglyNode
+	ValueInfo  string
+	Value      string
+	ValueComma string
+	NextInfo   string
+	Next       string
 }
 
 type SinglyLinkedList struct {
-  Head *Node
-  Tail *Node
-  Length int
+	HeadInfo  string
+	Head      *SinglyNode
+	HeadComma string
+	TailInfo  string
+	Tail      *SinglyNode
+	TailComma string
+	Length    int
 }
 
+func InitNode(v string) *SinglyNode {
+	return &SinglyNode{
+		ValueInfo:  "Value:",
+		Value:      v,
+		ValueComma: ",",
+		NextInfo:   "Next:",
+		Next:       "nil",
+	}
+}
 
-func InitSinglyLikedList() *SinglyLinkedList {
+func (n *SinglyNode) InitSinglyLikedList() *SinglyLinkedList {
+	return &SinglyLinkedList{
+		HeadInfo:  "Head:",
+		Head:      n,
+		HeadComma: ",",
+		TailInfo:  "Tail:",
+		Tail:      n,
+		TailComma: ",",
+		Length:    1,
+	}
+}
 
+func main() {
+	rootNode := InitNode("I am root")
+	ll := rootNode.InitSinglyLikedList()
+	fmt.Println(ll.Head)
 }
