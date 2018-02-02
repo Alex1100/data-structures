d6cc3f12187dff661d45bd8004a3b6e72cb48025
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2018-02-02 01:04:55 -0800

    [ADD] basic Graph Initialization and structs/types for initialization

--- go/graph.go
+++ go/graph.go
@@ -4,82 +4,64 @@ import (
   "fmt"
 )
 
-type DataValueStruct struct {
-  Value map[interface{}][]interface{}
-}
-
-type DataStruct struct {
-  DataInfo  string
-  DataValue *DataValueStruct
-  DataComma string
-}
-
-type NodesValueStruct struct {
-  Value []interface{}
-}
-
-type Node struct {
-  NodesArrayInfo  string
-  NodesArray      *NodesValueStruct
-  NodesArrayComma string
-}
-
-type EdgesValStruct struct {
-  Value []interface{}
-}
-
-type EdgesStruct struct {
-  EdgesInfo string
-  Edges     *EdgesValStruct
-}
-
 type Graph struct {
-  Data  *DataStruct
-  Nodes *Node
-  Edges *EdgesStruct
+  DataInfo  string
+  Data  map[interface{}][]interface{}
+  DataComma string
+  NodesArrayInfo  string
+  Nodes []interface{}
+  NodesArrayComma string
+  EdgesInfo string
+  Edges []interface{}
 }
 
 type gph *Graph
 
-func InitGraph(key interface{}) *Graph {
-  a := make(map[interface{}][]interface{});
-  b := make([]interface{}, 0);
+func InitGraph(node interface{}) *Graph {
+  d := make(map[interface{}][]interface{});
   c := make([]interface{}, 0);
-  d := make([]interface{}, 0);
-  b = append(b, key);
-  c = append(c, key);
-  a[key] = b;
-  
-  Val := &DataValueStruct{Value: a};
-  NodeVal := &NodesValueStruct{Value: c};
-  EdgesVal := &EdgesValStruct{Value: d};
-  
-  DataInstance := &DataStruct{
-    DataInfo: "Data:",
-    DataValue: Val,
-    DataComma: ",",
-  };
-  
-  NodeInstance := &Node{
-    NodesArrayInfo: "NodesArray:",
-    NodesArray: NodeVal,
-    NodesArrayComma: ",",
-  };
-  
-  EdgesInstance := &EdgesStruct{
-    EdgesInfo: "Edges:",
-    Edges: EdgesVal,
-  };
-  
+  a := make([]interface{}, 0);
+  b := make([]interface{}, 0);
+  c = append(c, node);
+  b = append(b, node);
+  d[node] = b;
+
   return &Graph{
-    Data: DataInstance,
-    Nodes: NodeInstance,
-    Edges: EdgesInstance,
+    DataInfo: "Data:",
+    Data: d,
+    DataComma: ",",
+    NodesArrayInfo: "NodesArray:",
+    Nodes: b,
+    NodesArrayComma: ",",
+    EdgesInfo: "Edges:",
+    Edges: a,
   };
+}
+
+
+func (g *Graph) AddNode(node interface{}) {
+  if g.Data[node] == nil {
+    newNode := make([]interface{}, 0);
+    newNode = append(newNode, node);
+    g.Data[node] = newNode;
+    g.Nodes = append(g.Nodes, node);
+  } else {
+    return;
+  }
 }
 
 func main() {
   go_graph := InitGraph("hello");
+  fmt.Println(go_graph)
+  fmt.Println("\nSHOULD RETURN TRUE: ", "hello" == go_graph.Data["hello"][0]);
+  fmt.Println("\nNEWLY ADDED NODE IS: ", go_graph.Nodes[len(go_graph.Nodes) - 1]);
   
-  fmt.Println("\nSHOULD RETURN TRUE: ", "hello" == go_graph.Data.DataValue.Value["hello"][0]);
+  go_graph.AddNode("bye");
+  
+  fmt.Println("\nNEWLY ADDED NODE IS: ", go_graph.Nodes[len(go_graph.Nodes) - 1]);
+  fmt.Println("\nSHOULD RETURN TRUE: ", go_graph.Data);
+  
+  go_graph.AddNode("bye");
+  
+  fmt.Println("\nSHOULD RETURN TRUE: ", go_graph.Data);
 }
